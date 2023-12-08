07b26ffc0bf0146b7e3aa9f8fc0fb3241bd54154
nurvoz <142998762+nurvoz@users.noreply.github.com>
2023-12-08 19:58:14 +0100

    Update e274.py

--- Ejercicios propuestos (Python ya)/Tema 79/e274.py
+++ Ejercicios propuestos (Python ya)/Tema 79/e274.py
@@ -1,7 +1,7 @@
 '''
 Crear un archivo de texto llamado 'datos.txt' con una codificación utf-8, almacenar tres líneas de texto. Abrir luego el archivo creado con el editor VS Code.
 '''
-archivo=open("datos.txt","w", encoding="utf-8") 
+archivo=open("datos7.txt","w", encoding="utf-8") 
 archivo.write("Línea 1\n") 
 archivo.write("Línea 2\n") 
 archivo.write("Línea 3\n")  
