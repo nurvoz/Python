9d6ae7c3cb0c6a9247f01348174511100da36560
nurvoz <142998762+nurvoz@users.noreply.github.com>
2023-12-08 19:57:27 +0100

    Update e270.py

--- Ejercicios propuestos (Python ya)/Tema 79/e270.py
+++ Ejercicios propuestos (Python ya)/Tema 79/e270.py
@@ -1,7 +1,7 @@
 '''
 Leer el contenido del archivo de texto 'datos.txt' línea a línea. (Con for)
 '''
-archivo=open("datos.txt","r")
+archivo=open("datos4.txt","r")
 for linea in archivo:
     print(linea, end='')
 archivo.close()
