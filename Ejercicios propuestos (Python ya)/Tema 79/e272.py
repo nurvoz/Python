913660030bacbb74dbcda5a91739210b4454e0b5
nurvoz <142998762+nurvoz@users.noreply.github.com>
2023-12-08 20:03:55 +0100

    Update e272.py

--- Ejercicios propuestos (Python ya)/Tema 79/e272.py
+++ Ejercicios propuestos (Python ya)/Tema 79/e272.py
@@ -1,7 +1,7 @@
 '''
 Abrir el archivo de texto 'datos.txt' y luego agregar 2 líneas. Imprimir luego el archivo completo.
 '''
-archivo=open("datos5.txt","a")
+archivo=open("datos1.txt","a")
 archivo.write("nueva línea 1\n")
 archivo.write("nueva línea 2\n")
 archivo.close()
